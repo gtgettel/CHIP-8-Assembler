150b2c809ec48aab70e104c46021e2545ad620d0
tucker <gtgett@gmail.com>
2017-01-21 01:22:10 -0500

    added JP

--- assembler.rs
+++ assembler.rs
@@ -7,6 +7,7 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::convert::AsRef;
 use std::process;
+use std::str;
 
 
 fn exit_compilation(mut writer: &File){
@@ -14,33 +15,73 @@ fn exit_compilation(mut writer: &File){
 }
 
 
-fn parse_line(line: String, mut writer: &File){
+fn check_less_than_4096(mut int: &str, mut writer: &File) -> bool{
+	int.to_string();
+	let int: u16 = int.parse().unwrap();
+	if int > 4095 {
+		return false;
+	}
+	else {
+		writer.write_fmt(format_args!("{:012b}", int));
+		return true; 
+	}
+}
+
+
+// TODO: track line number
+fn parse_line(mut line: String, mut writer: &File, ln: i64){
+	let mut line = str::replace(&line[..], ",", "");
 	let mut line = line.split(" ");
 	let words: Vec<&str> = line.collect();
 
 	match &words[0] as &str{
 		"SYS"  => {
 			if words.len() > 2 {
-				println!("Too many arguments: SYS addr");
+				println!("Line {}: Too many arguments, SYS", ln);
 				exit_compilation(writer);
-			}
-			let mut nnn = &words[1] as &str;
-			nnn.to_string();
-			let nnn_int: u16 = nnn.parse().unwrap();
-			if nnn_int > 4095 {
-				println!("Argument too large [addr]: SYS takes 12-bit address");
+			} else if words.len() <= 0 {
+				println!("Line {}: Too few arguments, SYS", ln);
 				exit_compilation(writer);
 			}
 			writer.write_fmt(format_args!("{:04b}", 0x0));
-			writer.write_fmt(format_args!("{:012b}", nnn_int));
+			if check_less_than_4096(&words[1] as &str, writer) {
+				println!("Line {}: Argument outsize of address range, SYS takes 12-bit address", ln);
+				exit_compilation(writer);
+			}
+			
 		} 
 		"CLS"  => {
 			writer.write_fmt(format_args!("{:016b}", 0x00E0));
 		}
 		"RET"  => {
-			writer.write_fmt(format_args!("{:016b}", 0x00EE))
+			writer.write_fmt(format_args!("{:016b}", 0x00EE));
 		}
-		// "JP"   => ,
+		"JP"   => {
+			if words.len() > 3 {
+				println!("Line {}: Too many arguments, JP", ln);
+				exit_compilation(writer);
+			} else if words.len() == 3 {
+				if str::contains(&words[1],"V") {
+					writer.write_fmt(format_args!("{:04b}", 0xB));
+					if check_less_than_4096(&words[2] as &str, writer) {
+						println!("Line {}: Argument outsize of address range, JP takes 12-bit address", ln);
+						exit_compilation(writer);
+					}
+				} else {
+					println!("Line {}: First argument is not register, JP", ln);
+					exit_compilation(writer);
+				}
+			} else if words.len() == 2 {
+				writer.write_fmt(format_args!("{:04b}", 0x1));
+				if check_less_than_4096(&words[1] as &str, writer) {
+					println!("Line {}: Argument outsize of address range, JP takes 12-bit address", ln);
+					exit_compilation(writer);
+				}
+			} else {
+				println!("Line {}: Too few arguments, JP", ln);
+				exit_compilation(writer);
+			}
+		}
 		// "CALL" => ,
 		// "SE"   => ,
 		// "SNE"  => ,
@@ -64,13 +105,14 @@ fn parse_line(line: String, mut writer: &File){
 		// "LOW"  => ,
 		// "HIGH" => ,
 		_      => {
-			println!("Unrecognized command: {}", &words[0]); // handle the rest of the cases
+			println!("Line {1}: Unrecognized command: {0}", &words[0], ln); // handle the rest of the cases
 		}
 	};
 }
 
 
 fn main(){
+	let mut ln: i64 = 0;
 	// parse command line
 	let args: Vec<String> = env::args().collect();
 	let mut path = Path::new(&args[1]);
@@ -91,6 +133,7 @@ fn main(){
   // Read the input file line by line
   let file = BufReader::new(file);
 	for line in file.lines() {
-    parse_line(line.unwrap(), &fileBinary);
+		ln += 1;
+    parse_line(line.unwrap(), &fileBinary, ln);
 	}
 }
